8a4ba94481708f8a8b9f5b5cef2334154908d27c
Tim Scanlin <tscanlin@gmail.com>
2016-02-23 18:37:12 -0800

    get hours to show up programatically

--- src/calendar/js/index.jsx
+++ src/calendar/js/index.jsx
@@ -1,7 +1,16 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import data from 'json!../data/day_1.json';
+import Day from './day.jsx';
+// console.log(Day, data)
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div className="grid">
+    {data.timeslots.map((slot) => {
+      // "key" is needed in addition to "id" since key is used for tracking and
+      // isn't passed down, but id can be passed down to a child component.
+      return <Day key={slot.id} id={slot.id}></Day>
+    })}
+  </div>,
   document.getElementById('example')
 );
